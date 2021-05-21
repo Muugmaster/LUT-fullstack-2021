27d3c1c17f8500c34d4d9e739ea31eb4da11816a
Muukkonen <niko@muukkonen.info>
2021-05-21 15:10:03 +0300

    Install boostrap and fixing

--- project/mean-todo-app/client/src/app/app.component.ts
+++ project/mean-todo-app/client/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'client';
